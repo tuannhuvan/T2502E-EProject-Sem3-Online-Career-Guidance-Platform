869cd36632507f494a15816b7f7035a2d703bc09
tuannhuvan <technology6789@gmail.com>
2026-06-10 16:05:59 +0700

    feat: initialize ASP.NET Core MVC project with MySQL provider and standard folder structure

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Career_Guidance_Platform.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
