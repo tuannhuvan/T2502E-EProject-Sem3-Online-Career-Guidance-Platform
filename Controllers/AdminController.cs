c6082beb04c44c4960064455a870aa316420707b
Nguyen Van Linh <nhoi03@gmail.com>
2026-06-12 19:53:19 +0700

    trang admin tạm thời tại dow admin template đang bị lỗi

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Career_Guidance_Platform.Models;
+
+namespace Career_Guidance_Platform.Controllers;
+
+public class AdminController : Controller
+{
+    public IActionResult Dashboard() => View();
+    public IActionResult Users() => View();
+    public IActionResult Mentors() => View();
+    public IActionResult CareerTests() => View();
+    public IActionResult Jobs() => View();
+    public IActionResult Resources() => View();
+    public IActionResult Reports() => View();
+    public IActionResult Settings() => View();
+
+
+    public IActionResult Terms() => View();
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
