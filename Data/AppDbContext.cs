b81fff187aa051da912f0a226916f9a6918cb902
Nguyen Van Linh <nhoi03@gmail.com>
2026-06-17 18:58:43 +0700

    seeding dữ liệu

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,6 +1,25 @@
+using Career_Guidance_Platform.Data.SeedData;
+using Career_Guidance_Platform.Models;
+using Career_Guidance_Platform.Models.SeedData;
+using Microsoft.EntityFrameworkCore;
+
 namespace Career_Guidance_Platform.Data;
 
-public class AppDbContext
+public class AppDbContext : DbContext
 {
-    
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<CareerPath> CareerPaths { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Category>().HasData(CategorySeed.Data);
+        modelBuilder.Entity<CareerPath>().HasData(CareerPathSeed.Data);
+    }
 }
