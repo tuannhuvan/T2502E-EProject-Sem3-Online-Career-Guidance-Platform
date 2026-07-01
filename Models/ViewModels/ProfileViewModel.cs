5c9136927b554e1e91f31a9e784349605fcbc2a4
tuannhuvan <technology6789@gmail.com>
2026-07-01 16:15:51 +0700

    feat(job-board): implement a personalized job feed and an application tracker

--- Models/ViewModels/ProfileViewModel.cs
+++ Models/ViewModels/ProfileViewModel.cs
@@ -7,4 +7,5 @@ public class ProfileViewModel
     public User User { get; set; } = null!;
     public List<TestResult> TestResults { get; set; } = new();
     public List<Resume> Resumes { get; set; } = new();
+    public List<JobApplication> JobApplications { get; set; } = new();
 }
